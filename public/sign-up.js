80c8b07462659be99344eb4b2221459d0e91a445
Matheus <matheusmeirelles.js>
2023-06-23 17:52:04 -0300

    Changes: * Emails checked by Deep-Email-Validator before getting to 2fa; * Inputs are now automatically reseted after pressing the send buttom; * New requisites on /sign up.
    Working at sending the pin code on 2fa and adding user to the database.

---
+++ public/sign-up.js
@@ -0,0 +1,56 @@
+let email = document.getElementById("email")
+let password = document.getElementById("password")
+let username = document.getElementById("username")
+
+function validInfo() {
+    //Input values
+    let vemail = document.getElementById("email").value
+    let vpassword = document.getElementById("password").value
+    let vusername = document.getElementById("username").value
+    //Regex for checking if data is in the right format before accepting
+    const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#@$!])[a-zA-Z\d#@$!]{8,}$/;
+    const usernameRegex = /^[a-zA-Z0-9_-]{3,32}$/
+    //Cleaning Inputs
+    email.value = null
+    password.value = null
+    username.value = null
+    //Boolens to check the validation
+    const isValidEmail = emailRegex.test(vemail)
+    const isValidPassword = passwordRegex.test(vpassword)
+    const isValidUsername = usernameRegex.test(vusername)
+    console.log(isValidEmail, isValidPassword, isValidUsername)
+
+    if (isValidEmail && isValidPassword && isValidUsername) {
+        checkEmail(vemail)
+    }
+}
+//Checks if the email really exixts before sending to 2fa
+function checkEmail(email) {
+    let valid = false
+    fetch("/emailValidator", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({email})
+    })
+    .then((res) => {
+        if (res.ok) {
+            console.log(res)
+            valid = true
+            res.json()
+            
+        }
+        else {
+            console.log("User not found", res)
+        }
+    })
+    .then((data) => {
+        if (valid) {
+            alert("Valid email", data)
+            window.location.href = "http://localhost:3000/2fa"
+        }
+        
+    })
+}
